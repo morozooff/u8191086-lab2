fbc2d844dec25f21ead29d6c79670d12e698b05a
morozooff <yar1@mail.ru>
2022-05-31 02:36:17 +0300

    Articles&&Tags| TASK-3: realizing command tag:count {id}

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,12 +13,14 @@ class ArticleController extends Controller
     public function index(Request $request): \Illuminate\Contracts\View\View
     {
         $filterParam = $request->input("tag");
-        $articles = Article::all();
+        
         if ($filterParam) {
-                $filteredArticles = Article::whereHas('tags', function($q) use ($filterParam) {
+                $filteredArticles = Article::whereHas('tags', function ($q) use ($filterParam) {
                     $q->where('name', '=', $filterParam);
                 })->get();
             $articles = $filteredArticles;
+        }  else {
+            $articles = Article::all();
         }
         return View::make('index')
             ->with('articles', $articles);
@@ -26,7 +28,7 @@ class ArticleController extends Controller
 
     public function show($code)
     {
-        $article = Article::all()->firstWhere('code', $code);
+        $article = Article::firstWhere('code', $code);
         return View::make('show')->with('article', $article);
     }
 }
